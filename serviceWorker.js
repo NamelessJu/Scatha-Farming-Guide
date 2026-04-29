a69ea21187d8d2b91b97ce21b7496c66dfe7b032
NamelessJu <julidom.merk@gmail.com>
2026-04-29 20:38:46 +0200

    chore: bump guide cache to v5

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,6 @@
 
 // Bump either of these to invalidate caches when changes are made
-const CACHE_GUIDE = 'guide-v4';
+const CACHE_GUIDE = 'guide-v5';
 const CACHE_PAGE = 'page-v5';
 
 const BASE_PATH = '/Scatha-Farming-Guide/';
