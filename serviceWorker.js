fedd82f6e35db53136070d3e8cb41381db0d2627
NamelessJu <68269467+NamelessJu@users.noreply.github.com>
2026-04-30 08:52:07 +0200

    fix: update notice may show on first load

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 
 // Bump either of these to invalidate caches when changes are made
 const CACHE_GUIDE = 'guide-v5';
-const CACHE_PAGE = 'page-v6';
+const CACHE_PAGE = 'page-v7';
 
 const BASE_PATH = '/Scatha-Farming-Guide/';
 
