25c771bc573b16db32b6424f443bd85fd9348f01
NamelessJu <julidom.merk@gmail.com>
2026-07-14 18:53:03 +0200

    bump version numbers

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 
 // Bump either of these to invalidate caches when changes are made
-const CACHE_GUIDE = 'guide-v7';
-const CACHE_PAGE = 'page-v9';
+const CACHE_GUIDE = 'guide-v8';
+const CACHE_PAGE = 'page-v10';
 
 const BASE_PATH = '/Scatha-Farming-Guide/';
 
