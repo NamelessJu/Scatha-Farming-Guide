c8f356cd73133bc3c35a1b59765d0d9d3ab4be78
NamelessJu <julidom.merk@gmail.com>
2026-08-08 15:54:49 +0200

    bump caches

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 
 // Bump either of these to invalidate caches when changes are made
-const CACHE_GUIDE = 'guide-v11';
-const CACHE_PAGE = 'page-v13';
+const CACHE_GUIDE = 'guide-v12';
+const CACHE_PAGE = 'page-v14';
 
 const BASE_PATH = '/Scatha-Farming-Guide/';
 
